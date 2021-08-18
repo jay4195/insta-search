40132bc0101aaee01c9c6b11a43a96f147f9a5a2
ZHIYUAN WANG(IT) <jay4195@qq.com>
2021-08-18 15:13:05 +0800

    minor submit

--- src/main/java/com/jay/instasearch/pojo/User.java
+++ src/main/java/com/jay/instasearch/pojo/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import java.util.Date;
 
@@ -15,25 +14,18 @@ public class User {
     @JsonProperty("_id")
     private Long id;
 
-    //@Length(max = 75, message = "avatar should less than 50")
     private String avatar;
 
-    @Length(max = 25, message = "username should less than 25")
     private String username;
 
-    @Length(max = 25, message = "password should less than 25")
     private String password;
 
-    @Length(max = 25, message = "fullname should less than 25")
     private String fullname;
 
-    @Length(max = 50, message = "email should less than 50")
     private String email;
 
-    @Length(max = 50, message = "website should less than 50")
     private String website;
 
-    @Length(max = 100, message = "bio should less than 50")
     private String bio;
 
     private Date createdAt;
--- src/main/java/com/jay/instasearch/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/jay/instasearch/service/impl/PostSearchServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -81,7 +80,6 @@ public class PostSearchServiceImpl implements PostSearchService {
         log.info("Search Posts By Caption records: {}", searchResults.size());
         return searchResults;
     }
-
 
     @Override
     public List<SearchSchema> getPostsByHashtags(List<String> hashtags) {
